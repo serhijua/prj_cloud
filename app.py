81961eb12034a96b85ade1eca5f0d3d2ae6aa980
serhijua <serhijovcharenko@gmail.com>
2016-06-14 19:15:21 +0200

    bla

---
+++ app.py
@@ -0,0 +1,29 @@
+import boto3, json, time
+from mail import send_email
+from uuid import uuid4
+from creator import create
+
+s3 = boto3.resource('s3')
+def upload_s3(source_file, filename):
+  bucket_name = '171422-serhij'
+  destination_filename = "albums/%s/%s" % (uuid4().hex, filename)
+  print destination_filename
+  bucket = s3.Bucket(bucket_name)
+  bucket.put_object(Key=destination_filename, Body=source_file, ACL='public-read')
+  return destination_filename
+
+sqs = boto3.resource('sqs')
+albumRequests = sqs.get_queue_by_name(QueueName='test123')
+bucket_address = 'https://s3.eu-central-1.amazonaws.com/171422-serhij'
+
+while True:
+  for albumRequest in albumRequests.receive_messages():
+    print('processing request ..........')
+    albumData = json.loads(albumRequest.body)
+    print albumData
+    pdf = create(albumData)
+    dest = upload_s3(pdf.getvalue(), 'album.pdf')
+    send_email(albumData['sent_to'], 'Your album', 'download here: %s/%s' % (bucket_address, dest))
+    albumRequest.delete()
+    print('request processing finished [X]')
+  time.sleep(1)
---
+++ mail.py
@@ -0,0 +1,38 @@
+
+import boto3
+
+session = boto3.session.Session()
+client = session.client('ses', region_name='eu-west-1')
+
+def send_email(to, subject, body):
+    response = client.send_email(
+        Source='maultosar@gmail.com',
+        Destination={
+            'ToAddresses': [
+                to,
+            ]
+        },
+        Message={
+            'Subject': {
+                'Data': subject,
+                'Charset': 'utf-8'
+            },
+            'Body': {
+                'Text': {
+                    'Data': body,
+                    'Charset': 'utf-8'
+                },
+                'Html': {
+                    'Data': body,
+                    'Charset': 'utf-8'
+                }
+            }
+        },
+        ReplyToAddresses=[
+            'maultosar@gmail.com',
+        ],
+        ReturnPath='maultosar@gmail.com'
+    )
+    return response
+
+
